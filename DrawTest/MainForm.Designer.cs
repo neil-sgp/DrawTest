84766aa1d5e62947f25ab4fea78ee771acfcb29c
Neil Stoker <neil@sgpconsulting.co.uk>
2016-09-15 09:42:32 +0100

    Setup initial events

--- DrawTest/MainForm.Designer.cs
+++ DrawTest/MainForm.Designer.cs
@@ -33,12 +33,18 @@
             this.label1 = new System.Windows.Forms.Label();
             this.drawLine = new System.Windows.Forms.Button();
             this.drawOthers = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picturePanel = new System.Windows.Forms.Panel();
+            this.pbPageImage = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusMessage = new System.Windows.Forms.ToolStripStatusLabel();
+            this.fromLocation = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toLocation = new System.Windows.Forms.ToolStripStatusLabel();
+            this.hitCheck = new System.Windows.Forms.ToolStripStatusLabel();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.picturePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPageImage)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -49,15 +55,14 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.panel1, 2, 1);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.picturePanel, 2, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(23, 24);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(936, 578);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(936, 384);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -77,7 +82,7 @@
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(200, 532);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(200, 338);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
             // label1
@@ -112,37 +117,89 @@
             this.drawOthers.UseVisualStyleBackColor = true;
             this.drawOthers.Click += new System.EventHandler(this.drawOthers_Click);
             // 
-            // panel1
+            // picturePanel
             // 
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(229, 23);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(684, 532);
-            this.panel1.TabIndex = 1;
+            this.picturePanel.Controls.Add(this.pbPageImage);
+            this.picturePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picturePanel.Location = new System.Drawing.Point(229, 23);
+            this.picturePanel.Name = "picturePanel";
+            this.picturePanel.Size = new System.Drawing.Size(684, 338);
+            this.picturePanel.TabIndex = 1;
             // 
-            // pictureBox1
+            // pbPageImage
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(100, 85);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(322, 312);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pbPageImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pbPageImage.Location = new System.Drawing.Point(12, 13);
+            this.pbPageImage.Name = "pbPageImage";
+            this.pbPageImage.Size = new System.Drawing.Size(182, 170);
+            this.pbPageImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPageImage.TabIndex = 0;
+            this.pbPageImage.TabStop = false;
+            this.pbPageImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbPageImage_MouseDown);
+            this.pbPageImage.MouseEnter += new System.EventHandler(this.pbPageImage_MouseEnter);
+            this.pbPageImage.MouseLeave += new System.EventHandler(this.pbPageImage_MouseLeave);
+            this.pbPageImage.MouseHover += new System.EventHandler(this.pbPageImage_MouseHover);
+            this.pbPageImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbPageImage_MouseMove);
+            this.pbPageImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbPageImage_MouseUp);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusMessage,
+            this.fromLocation,
+            this.toLocation,
+            this.hitCheck});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 450);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1036, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusMessage
+            // 
+            this.statusMessage.Name = "statusMessage";
+            this.statusMessage.Size = new System.Drawing.Size(118, 17);
+            this.statusMessage.Text = "toolStripStatusLabel1";
+            // 
+            // fromLocation
+            // 
+            this.fromLocation.Name = "fromLocation";
+            this.fromLocation.Size = new System.Drawing.Size(38, 17);
+            this.fromLocation.Text = "From:";
+            this.fromLocation.Visible = false;
+            // 
+            // toLocation
+            // 
+            this.toLocation.Name = "toLocation";
+            this.toLocation.Size = new System.Drawing.Size(21, 17);
+            this.toLocation.Text = "to:";
+            this.toLocation.Visible = false;
+            // 
+            // hitCheck
+            // 
+            this.hitCheck.Name = "hitCheck";
+            this.hitCheck.Size = new System.Drawing.Size(82, 17);
+            this.hitCheck.Text = "outside image";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(936, 578);
+            this.ClientSize = new System.Drawing.Size(1036, 472);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "MainForm";
             this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.picturePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPageImage)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -153,8 +210,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button drawLine;
         private System.Windows.Forms.Button drawOthers;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel picturePanel;
+        private System.Windows.Forms.PictureBox pbPageImage;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusMessage;
+        private System.Windows.Forms.ToolStripStatusLabel fromLocation;
+        private System.Windows.Forms.ToolStripStatusLabel toLocation;
+        private System.Windows.Forms.ToolStripStatusLabel hitCheck;
     }
 }
 
--- DrawTest/MainForm.cs
+++ DrawTest/MainForm.cs
@@ -12,19 +12,139 @@ namespace DrawTest
 {
     public partial class MainForm : Form
     {
+        private bool _drawLineMode { get; set; } = false;
+        private Bitmap pageImage { get; set; }
+        private Point _startPoint { get; set; }
+        private int ZoomLevel { get; set; } = 100;
+        private bool ZoomToFit { get; set; } = true;
+
+
+
         public MainForm()
         {
             InitializeComponent();
+
+            picturePanel.AutoScroll = false;
+            resizeImage();
+        }
+
+        private void DisplayPage()
+        {
+            // Load the image
+            pageImage = Resource1.FrontPage;
+
+            // Draw markers
+
+            pbPageImage.Image = pageImage;
         }
 
         private void drawLine_Click(object sender, EventArgs e)
         {
-
+            _drawLineMode = true;
+            statusMessage.Text = "Draw line mode: hold the mouse button down to draw the line";
         }
 
         private void drawOthers_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void resizeImage()
+        {
+            if(ZoomToFit)
+            {
+                picturePanel.AutoScroll = false;
+                Size panelSize = picturePanel.Size;
+                pbPageImage.Size = panelSize;
+            }
+            else
+            {
+                picturePanel.AutoScroll = true;
+                pbPageImage.Width = ZoomLevel * picturePanel.Width / 100;
+                pbPageImage.Height = ZoomLevel * picturePanel.Height / 100;
+            }
+
+            DisplayPage();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            resizeImage();
+        }
+
+        private void pbPageImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(_drawLineMode)
+            {
+                _startPoint = e.Location;
+                statusMessage.Text = "Draw line mode: release the mouse button to finish drawing the line";
+                fromLocation.Text = "From: " + e.Location.ToString();
+                fromLocation.Visible = true;
+                toLocation.Visible = true;
+            }
+        }
+
+        private void pbPageImage_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbPageImage_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbPageImage_MouseLeave(object sender, EventArgs e)
+        {
+            if(_drawLineMode)
+            {
+                statusMessage.Text = "Draw line mode cancelled: mouse moved outside the image area";
+                _drawLineMode = false;
+            }
+        }
+
+        private void pbPageImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if( _drawLineMode)
+            {
+                toLocation.Text = "to: " + e.Location.ToString();
+            }
+
+            if(insideImage(e.Location))
+            {
+                hitCheck.Text = "Inside";
+            }
+            else
+            {
+                hitCheck.Text = "Outside";
+            }
+        }
+
+        private void pbPageImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(_drawLineMode)
+            {
+                Point endPoint = e.Location;
+                endPoint.Y = _startPoint.Y;
+                _drawLineMode = false;
+                toLocation.Text = "to: " + endPoint.ToString();
+                statusMessage.Text = "Draw line mode ended: mouse button released";
+            }
+        }
+
+
+
+        /// <summary>
+        /// Check to see if a point is inside the actual image
+        /// </summary>
+        /// 
+        /// <param name="point">The point to check</param>
+        /// <returns>True if the point is inside the image, false otherwise</returns>
+        /// 
+        private bool insideImage(Point point)
+        {
+
+            return false;
         }
     }
 }
